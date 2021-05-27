bb71fd1b276a928ceb5cb99eba458ddca657da8d
rahi519 <84790590+rahi519@users.noreply.github.com>
2021-05-27 14:51:49 +0530

    Add files via upload

---
+++ Day2/switch.php
@@ -0,0 +1,17 @@
+<?php
+$a=1;
+
+switch($a){
+	case 1: 
+		echo "ONE";
+		break;
+	Case 2:
+		echo "TWO";
+		break;
+	Case 3:
+		echo "THREE";
+		break;
+	default;
+}
+
+?>
---
+++ Day2/variabledemo.php
@@ -0,0 +1,27 @@
+<?php
+
+//variable
+//Method 
+
+$a=10;
+$b=20;
+
+//method 1
+echo"A value is.$a";
+echo"<br/>B value is.$b";
+
+//method2
+echo"<br/>A value is.$a"."<br/>B value is.$b";
+
+//method 3
+echo"<br/>A value is$a<br/>B value is$b";
+
+//method 4
+echo'<br/>A value is$a<br/>B value is$b'; 
+
+//method 5
+echo"<br/>A value is$a<br/>B value is$b </br>Sum is:".($a+$b); 
+
+
+
+?>
